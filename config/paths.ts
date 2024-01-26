229423e836c0680d4d7499f8c87943571a83d067
deeps.dev <deepak.garasangi@gmail.com>
2024-01-26 13:19:09 +0530

    Refactor user authentication form and update paths

--- config/paths.ts
+++ config/paths.ts
@@ -1,11 +1,13 @@
 export type TPathRouteMap = {
   HOME: string;
   LOGIN: string;
+  DASHBOARD: string;
 };
 
 export const PATHS: TPathRouteMap = {
   HOME: '/',
   LOGIN: '/login',
+  DASHBOARD: '/dashboard',
 };
 
 export const PATHS_FOR_SITEMAP = [PATHS.HOME, PATHS.LOGIN];
