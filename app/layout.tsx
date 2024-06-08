8e6ae45477a2da6ea121295956853af522a2f15a
deeps.dev <deepak.garasangi@gmail.com>
2024-06-08 12:17:36 +0530

    Add Analytics component to RootLayout and update environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { Metadata } from 'next';
 
+import { Analytics } from '@/components/analytics';
 import { SiteHeader } from '@/components/site-header';
 import { TailwindIndicator } from '@/components/tailwind-indicator';
 import { ThemeProvider } from '@/components/theme-provider';
@@ -39,6 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
+            <Analytics />
             <TailwindIndicator />
           </ThemeProvider>
         </body>
---
+++ components/analytics.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Script from 'next/script';
+
+// Uses Google Analytics, replace with your own analytics provider
+
+export function Analytics() {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id="" strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+    </>
+  );
+}
