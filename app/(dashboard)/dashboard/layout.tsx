ec3c220c0819c0016aa6b1d4982a5262ace375c7
deeps.dev <deepak.garasangi@gmail.com>
2024-06-23 16:09:55 +0530

    update page metadata

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Forms',
-  description: 'Advanced form example using react-hook-form and Zod.',
+  title: 'Dashboard',
+  description: 'Dashboard example using Next.js 14',
 };
 
 interface SettingsLayoutProps {
