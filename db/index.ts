bedaa5cea7a7462817ba60499077eddaba7860d6
deeps.dev <deepak.garasangi@gmail.com>
2024-11-03 15:10:28 +0530

    add

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,12 @@
-import 'dotenv/config'
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core"
-import { drizzle } from "drizzle-orm/node-postgres"
-import { Pool } from "pg"
-import { pgDbConfig } from "@/config/pg"
+import { pgDbConfig } from '@/config/pg';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
 const pool = new Pool({
-    connectionString: pgDbConfig.connectionString!,
-    max: 20,
-    idleTimeoutMillis: pgDbConfig.idleTimeoutMillis,
-    connectionTimeoutMillis: pgDbConfig.connectionTimeoutMillis,
-})
-export const db = drizzle({ client: pool, casing: 'snake_case' })
+  connectionString: pgDbConfig.connectionString!,
+  max: 20,
+  idleTimeoutMillis: pgDbConfig.idleTimeoutMillis,
+  connectionTimeoutMillis: pgDbConfig.connectionTimeoutMillis,
+});
+export const db = drizzle({ client: pool, casing: 'snake_case' });
--- db/schema.ts
+++ db/schema.ts
@@ -1,89 +1,78 @@
 import {
-    timestamp,
-    pgTable,
-    text,
-    primaryKey,
-    integer,
-} from "drizzle-orm/pg-core"
-import { sql } from 'drizzle-orm'
-import { ProviderType } from "next-auth/providers"
-import { createInsertSchema } from "drizzle-zod"
-import { z } from "zod"
+  integer,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema } from 'drizzle-zod';
+import { ProviderType } from 'next-auth/providers';
+import { z } from 'zod';
+import { timestamps } from './column.helpers';
 
-const timestamps = {
-    created_at: timestamp('created_at')
-        .defaultNow()
-        .notNull(),
-    updated_at: timestamp('updated_at')
-        .defaultNow()
-        .notNull()
-        .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-    deleted_at: timestamp('deleted_at')
-}
+export const users = pgTable('user', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text('name'),
+  email: text('email').unique(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+  ...timestamps,
+});
 
-export const users = pgTable("user", {
-    id: text("id")
-        .primaryKey()
-        .$defaultFn(() => crypto.randomUUID()),
-    name: text("name"),
-    email: text("email").unique(),
-    emailVerified: timestamp("emailVerified", { mode: "date" }),
-    image: text("image"),
-    ...timestamps
-})
-
-export const usersSchema = createInsertSchema(users)
-export type UserSchema = z.infer<typeof usersSchema>
+export const usersSchema = createInsertSchema(users);
+export type UserSchema = z.infer<typeof usersSchema>;
 
 export const accounts = pgTable(
-    "account",
-    {
-        userId: text("userId")
-            .notNull()
-            .references(() => users.id, { onDelete: "cascade" }),
-        type: text("type").$type<ProviderType>().notNull(),
-        provider: text("provider").notNull(),
-        providerAccountId: text("providerAccountId").notNull(),
-        refresh_token: text("refresh_token"),
-        access_token: text("access_token"),
-        expires_at: integer("expires_at"),
-        token_type: text("token_type"),
-        scope: text("scope"),
-        id_token: text("id_token"),
-        session_state: text("session_state"),
-        ...timestamps
-    },
-    (account) => {
-        return {
-            accountsPkey: primaryKey({
-                columns: [account.provider, account.providerAccountId],
-            }),
-        }
-    }
-)
+  'account',
+  {
+    userId: text('userId')
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: text('type').$type<ProviderType>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+    ...timestamps,
+  },
+  (account) => {
+    return {
+      accountsPkey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    };
+  },
+);
 
-export const sessions = pgTable("session", {
-    sessionToken: text("sessionToken").primaryKey(),
-    userId: text("userId")
-        .notNull()
-        .references(() => users.id, { onDelete: "cascade" }),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-    ...timestamps
-})
+export const sessions = pgTable('session', {
+  sessionToken: text('sessionToken').primaryKey(),
+  userId: text('userId')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  expires: timestamp('expires', { mode: 'date' }).notNull(),
+  ...timestamps,
+});
 
 export const verificationTokens = pgTable(
-    "verificationToken",
-    {
-        identifier: text("identifier").notNull(),
-        token: text("token").notNull(),
-        expires: timestamp("expires", { mode: "date" }).notNull(),
-        ...timestamps
-    },
-    (table) => {
-        return {
-            verificationTokenPkey: primaryKey({
-                columns: [table.identifier, table.token],
-            }),
-        }
-    }
-)
+  'verificationToken',
+  {
+    identifier: text('identifier').notNull(),
+    token: text('token').notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
+    ...timestamps,
+  },
+  (table) => {
+    return {
+      verificationTokenPkey: primaryKey({
+        columns: [table.identifier, table.token],
+      }),
+    };
+  },
+);
