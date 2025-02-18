3bd4a5bb2c865fec5a26d70773213f2d03463d36
deeps.dev <deepak.garasangi@gmail.com>
2025-02-18 16:05:34 +0530

    add next js

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function getInitials(name: string | null | undefined): string {
-  if (!name) return '';
-
-  const nameParts = name.split(' ');
-  const firstInitial = nameParts[0]?.charAt(0).toUpperCase() || '';
-  const secondInitial =
-    nameParts.length > 1 ? nameParts[1]?.charAt(0).toUpperCase() : '';
-
-  return `${firstInitial}${secondInitial || firstInitial}`;
+  return twMerge(clsx(inputs))
 }
