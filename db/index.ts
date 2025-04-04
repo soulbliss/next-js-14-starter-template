fc6fdd332ea94c7f6cba5baaacc4d8b8c4612ff5
deeps.dev <deepak.garasangi@gmail.com>
2025-04-04 10:26:48 +0530

    Enable SSL for database connection in index.ts

--- db/index.ts
+++ db/index.ts
@@ -8,5 +8,6 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: pgDbConfig.idleTimeoutMillis,
   connectionTimeoutMillis: pgDbConfig.connectionTimeoutMillis,
+  ssl: true,
 });
 export const db = drizzle({ client: pool, casing: 'snake_case' });
